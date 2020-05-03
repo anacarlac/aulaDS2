3b09e35eb4f7fbad95f21431a0c9b1fe9060c8bd
Ana <anacarlacrispim@gmail.com>
2020-05-03 19:40:56 -0300

    Initial commit

---
+++ colar.R
@@ -0,0 +1,19 @@
+#' Colar dois valores
+#'
+#' Recebe um valor x e outro y e retorna os dois valores com um espaço de separação
+#'
+#' @param x primeiro argumento
+#' @param y segundo argumento
+#'
+#' @return os dois valores com um espaço de separação
+#'
+#' @export
+#'
+
+colar <- function(x, y) {
+  paste(x, y)
+}
+
+
+
+
